1616291098eaca6be04bc6ab309279e30ccad52e
Daniel Bishop <DBishop.rwc@gmail.com>
2015-10-10 20:21:46 -0400

    fixed

--- Lab1/logic.v
+++ Lab1/logic.v
@@ -1,12 +1,13 @@
 `define AND and #20
 `define OR or #20
+`define NOT not #20
 
 module ander(
   output c,
   input a,
   input b
 );
-  `AND a0(c, a, b);
+  `AND(c, a, b);
 endmodule
 
 module orrer(
@@ -15,6 +16,13 @@ module orrer(
   input b
 );
   `OR(c, a, b);
+endmodule
+
+module inverter(
+	output out,
+	input a
+);
+	`NOT(out, a);
 endmodule
 
 module bitwiseAND(
@@ -55,17 +63,40 @@ module bitwiseOR(
   end
 endmodule
 
+module bitwiseINV(
+	output reg[31:0] out,
+	input[31:0] a
+);
+	reg p;
+	wire k;
+	inverter myInverter(k, p);
+	integer i;
+	initial begin
+		#26; //25 WONT WORK NUH UH NO SIR
+		for (i = 0; i < 32; i = i + 1) begin
+			p = a[i];
+			#20;
+			out[i] = k;
+		end
+	end
+endmodule
+
 module test();
   reg [31:0] a = 32'sb00000000100001000001000000001100;
   reg [31:0] b = 32'sb00010000100000000001000000001100;
   wire [31:0] out;
   wire [31:0] out2;
+	wire [31:0] out3;
+	wire [31:0] out4;
   bitwiseAND myAnder(out, a, b);
   bitwiseOR myOrrer(out2, a, b);
+	bitwiseINV myAnderInverter(out3, out);
+	bitwiseINV myOrrerInverter(out4, out2);
   initial begin
-    #660;
-    $display("and %b", out);
-    #660;
-    $display("or  %b", out2);
+		#667;
+    $display("and     %b", out);
+		$display("not and %b", out3);
+    $display("or      %b", out2);
+		$display("not or  %b", out4);
   end
 endmodule
