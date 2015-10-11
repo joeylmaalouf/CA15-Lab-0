f03500b54ffa20a2ae467e3858a8e1c323a5c00e
Daniel Bishop <DBishop.rwc@gmail.com>
2015-10-10 20:37:44 -0400

    logicals complete
    this makes no sense

--- Lab1/logic.v
+++ Lab1/logic.v
@@ -1,6 +1,7 @@
 `define AND and #20
 `define OR or #20
 `define NOT not #20
+`define XOR xor #20
 
 module ander(
   output c,
@@ -25,6 +26,14 @@ module inverter(
 	`NOT(out, a);
 endmodule
 
+module xorrer(
+	output c,
+	input a,
+	input b
+);
+	`XOR(c, a, b);
+endmodule
+
 module bitwiseAND(
   output reg[31:0] out,
   input[31:0] a, b
@@ -34,7 +43,7 @@ module bitwiseAND(
   ander myAnder(k, placeholder_1, placeholder_2);
   integer i;
   initial begin
-    #5;
+    #0; //Lel this has to be here or it won't work    getrekt/10
     for (i = 0; i < 32; i = i + 1) begin
       placeholder_1 = a[i];
       placeholder_2 = b[i];
@@ -53,7 +62,7 @@ module bitwiseOR(
   orrer myOrrer(k, p1, p2);
   integer i;
   initial begin
-    #5;
+    #0; //Lel this has to be here or it won't work    getrekt/10
     for (i = 0; i < 32; i = i + 1) begin
       p1 = a[i];
       p2 = b[i];
@@ -81,6 +90,27 @@ module bitwiseINV(
 	end
 endmodule
 
+module bitwiseXOR(
+	output reg[31:0] out,
+	input[31:0] a,
+	input[31:0] b
+);
+	reg p1;
+	reg p2;
+	wire k;
+	integer i;
+	xorrer myXorrer(k, p1, p2);
+	initial begin
+		#0; //Lel this has to be here or it won't work    getrekt/10
+		for (i = 0; i < 32; i = i + 1) begin
+			p1 = a[i];
+			p2 = b[i];
+			#20;
+			out[i] = k;
+		end
+	end
+endmodule
+
 module test();
   reg [31:0] a = 32'sb00000000100001000001000000001100;
   reg [31:0] b = 32'sb00010000100000000001000000001100;
@@ -88,15 +118,18 @@ module test();
   wire [31:0] out2;
 	wire [31:0] out3;
 	wire [31:0] out4;
+	wire [31:0] out5;
   bitwiseAND myAnder(out, a, b);
   bitwiseOR myOrrer(out2, a, b);
 	bitwiseINV myAnderInverter(out3, out);
 	bitwiseINV myOrrerInverter(out4, out2);
+	bitwiseXOR myXorrer(out5, a, b);
   initial begin
 		#667;
     $display("and     %b", out);
 		$display("not and %b", out3);
     $display("or      %b", out2);
 		$display("not or  %b", out4);
+		$display("xor     %b", out5);
   end
 endmodule
