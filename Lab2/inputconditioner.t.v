a940bfadf84cc76759b0fb12e2a8cd2ed5bb3a74
unknown <gbutterick@1DPFG12.olin.edu>
2015-11-07 23:31:16 -0500

    Input Conditioner Test Bench now functional

--- Lab2/inputconditioner.t.v
+++ Lab2/inputconditioner.t.v
@@ -1,3 +1,4 @@
+`include "inputconditioner.v"
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
@@ -14,7 +15,7 @@ module testConditioner();
     			 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
-			 .negativeedge(falling))
+			 .negativeedge(falling));
 
 
     // Generate clock (50MHz)
@@ -22,35 +23,52 @@ module testConditioner();
     always #10 clk=!clk;    // 50MHz Clock
     
     initial begin
+    pin = 0;
+    #1
     // Your Test Code
     // Be sure to test each of the three conditioner functions:
     // Synchronize, Clean, Preprocess (edge finding)
     // Synchronize:
     // Give two inputs out of sync and see if it reads one before the other off clock cycle
+    pin = 1;
+    end
     always @(conditioned) begin
-	if clk != 1 begin
-	$display ("Conditioned not synchronized with clock")
+	if (clk != 1) begin
+	$display ("Conditioned not synchronized with clock");
 	end
     end
 
     // Input Stabilized:
+
     always @(pin) begin
+      #10
+      pin = 1;
+      #1
+      pin = 0;
+      #1
+      pin = 1;
       #100
-	if (pin == 1) begin
-          $display ("Debouncing Input Signal Ineffective")
+      if (rising == 1) begin
+      	if (conditioned != 1) begin
+          $display ("Debouncing Input Signal Ineffective");
+      	end
       end
     end
     //Clock Edge:
+    
     always @(rising) begin
-	if conditioned != 1 begin
-	  $display ("Rising edge failed to set conditioned to 1")
-	end
-    end
-    always @(falling) begin
-	if conditioned != 0 begin
-	  $display ("Falling edge failed to set conditioned to 0")
+	if (conditioned != 1) begin
+	  $display ("Rising edge failed to set conditioned to 1");
 	end
     end
     
+    always @(falling) begin
 	
+	if (conditioned != 0) begin
+	  $display ("Falling edge failed to set conditioned to 0");
+	  $display ("%b", conditioned);
+	end
+    end
+
+
 endmodule
