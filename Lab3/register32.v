03996dd558b6583d9ed6b988c289c1ed9707efd5
joeylmaalouf <joeylmaalouf@gmail.com>
2015-11-20 21:50:38 -0500

    formatting

--- Lab3/register32.v
+++ Lab3/register32.v
@@ -1,13 +1,13 @@
 module register32
 (
-output reg[0:31]  q,
-input[0:31]       d,
-input       wrenable,
-input       clk
+  output reg[0:31] q,
+  input[0:31]      d,
+  input            wrenable,
+  input            clk
 );
-    always @(posedge clk) begin
-        if(wrenable) begin
-            q = d;
-        end
+  always @(posedge clk) begin
+    if (wrenable) begin
+      q = d;
     end
+  end
 endmodule
